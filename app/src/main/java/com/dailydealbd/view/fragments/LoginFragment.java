71d9bcff67f0a852e31b04dcef0fa55e8f9abdc2
fitareq <fitareq@gmail.com>
2021-01-04 10:31:51 +0600

    station1

--- app/src/main/java/com/dailydealbd/view/fragments/LoginFragment.java
+++ app/src/main/java/com/dailydealbd/view/fragments/LoginFragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.dailydealbd.R;
+import com.google.android.material.textfield.TextInputEditText;
+
 
 public class LoginFragment extends Fragment {
 
@@ -16,10 +23,34 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private ImageButton backButton;
+    private TextInputEditText userPhoneNumber;
+    private TextInputEditText userPassword;
+    private Button loginButton;
+    private TextView forgotPassword;
+    private TextView registerTview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return v;
     }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+
+
+
+
 }
