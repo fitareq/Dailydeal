d9565c971881063d880a5d05360e4a7a06b19243
fitareq <fitareq@gmail.com>
2021-01-09 15:31:30 +0600

    station1

---
+++ app/src/main/java/com/dailydealbd/view/fragments/OrderFragment.java
@@ -0,0 +1,40 @@
+package com.dailydealbd.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dailydealbd.R;
+
+
+
+public class OrderFragment extends Fragment {
+
+
+
+
+    public OrderFragment() {
+        // Required empty public constructor
+        }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_order, container, false);
+        return v;
+    }
+
+
+
+
+}
