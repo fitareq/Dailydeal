1750df2088fe285591705826401825104091b29f
shumssunam <shumssunam15@gmail.com>
2021-01-20 16:43:58 +0600

    Station2

--- app/src/main/java/com/dailydealbd/view/fragments/ContactUsFragment.java
+++ app/src/main/java/com/dailydealbd/view/fragments/ContactUsFragment.java
@@ -1,9 +1,13 @@
 package com.dailydealbd.view.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,12 +18,34 @@ import com.dailydealbd.R;
 
 public class ContactUsFragment extends Fragment {
 
+    private TextView callbtn;
+    private ImageView mapbtn;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.contact_us,container,false);
+        callbtn = v.findViewById(R.id.map);
+        mapbtn = v.findViewById(R.id.phone_number);
+
+        mapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01705401056"));
+                startActivity(intent);
+            }
+        });
+
+        callbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q=Sultan Ahmed Plaza, Dhaka"));
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
 
