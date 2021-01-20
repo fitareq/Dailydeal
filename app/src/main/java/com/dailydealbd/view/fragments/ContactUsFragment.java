00f685015af0d408db1df34a0a9c18245addb53d
fitareq <fitareq@gmail.com>
2021-01-20 16:35:32 +0600

    station1

---
+++ app/src/main/java/com/dailydealbd/view/fragments/ContactUsFragment.java
@@ -0,0 +1,29 @@
+package com.dailydealbd.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.dailydealbd.R;
+
+
+public class ContactUsFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.contact_us,container,false);
+        return v;
+    }
+
+
+
+
+}
